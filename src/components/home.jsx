ff4b4213e4bfedcd3dd1fb5d621749b6d680df93
Sunil Bista <btwitsbista@gmail.com>
2023-03-09 14:13:01 +0545

    Refined the Design

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,115 +1,219 @@
-import React, { Component } from 'react'
-import { Button } from 'reactstrap'
-import axios from 'axios';
-import {Table} from 'reactstrap'
-import 'rsuite-table/lib/less/index.less';
-import { Navigate ,Redirect} from "react-router-dom";
-import { Alert } from '@coreui/react';
-import '@coreui/coreui/dist/css/coreui.min.css'
-import { CContainer } from '@coreui/react'
-
-
-
+import React, { Component } from "react";
+import { Button } from "reactstrap";
+import axios from "axios";
+import { Table } from "reactstrap";
+import "rsuite-table/lib/less/index.less";
+import { Navigate, Redirect } from "react-router-dom";
+import { Alert } from "@coreui/react";
+import "@coreui/coreui/dist/css/coreui.min.css";
+import { CContainer } from "@coreui/react";
+import PrintIcon from "@material-ui/icons/Print";
+import ME from "../assets/me.jpeg";
+import Document from "../assets/7.PNG";
+import { AiFillDelete, AiFillPrinter } from "react-icons/ai";
 export default class home extends Component {
-
   constructor(props) {
-    super(props)
-  this.state = {
-         traineedata:[],
-         view:false
-    }
-}
-  componentDidMount=()=>{
-   
-    axios.get('http://140.238.204.76:3001/trainee')
-    .then((res) => {
-        this.setState({
-            traineedata: res.data 
-        })
-        console.log(res.data);
-    }).catch((err) => console.log(err.response));
+    super(props);
+    this.state = {
+      traineedata: [
+        {
+          id: 1,
+          doc_type: "Loren",
+          name: "Sunil Bista",
+          phone: "123456",
+          type: "Loren",
+          created_on: "2023/23/2",
+          dob: "2058/05/01",
+          address: "Shantinagar",
+          fathers_name: "Sunil Bista",
+          voucher_id: 44,
+          profile_image: ME,
+          doc_image: Document,
+        },
+      ],
+      view: false,
+    };
   }
+  // componentDidMount = () => {
+  //   axios
+  //     .get("http://140.238.204.76:3001/trainee")
+  //     .then((res) => {
+  //       this.setState({
+  //         traineedata: res.data,
+  //       });
+  //       console.log(res.data);
+  //     })
+  //     .catch((err) => console.log(err.response));
+  // };
 
-  deleterecord=(record_id)=>{
-    var newarray=[];
-    axios.delete('http://140.238.204.76:3001/trainee/'+record_id)
-    .then((res) => {
-      res.data.forEach(element => {
-        if(element.id==record_id){
+  // deleterecord = (record_id) => {
+  //   var newarray = [];
+  //   axios
+  //     .delete("http://140.238.204.76:3001/trainee/" + record_id)
+  //     .then((res) => {
+  //       res.data.forEach((element) => {
+  //         if (element.id == record_id) {
+  //         } else {
+  //           newarray.push(element);
+  //         }
+  //       });
+  //       this.setState({
+  //         traineedata: newarray,
+  //       });
+  //     })
+  //     .catch((err) => console.log(err.response));
+  // };
 
-        }else{
-          newarray.push(element);
-
-        }
-      });
-      this.setState({
-        traineedata:newarray
-      })
-
-    }).catch((err) => console.log(err.response));
-  }
-
-printrow=async(data)=>{
-  localStorage.setItem('selected',data);
-  this.setState({
-    view :true
-  })
-};
+  printrow = async (data) => {
+    localStorage.setItem("selected", data);
+    this.setState({
+      view: true,
+    });
+  };
   render() {
-    if(this.state.view==true){
-
+    if (this.state.view == true) {
+      return <Navigate to="/view" replace={true} />;
+    } else if (this.state.traineedata != []) {
       return (
-        <Navigate to="/view" replace={true} />
-      )
-    }
+        <div className="overflow-x-auto scrollbar-hide">
+          <CContainer fluid>
+            <Table
+              name="datatable"
+              className="w-full border-collapse border border-gray-200"
+            >
+              <thead>
+                <tr>
+                  <th className="px-4 py-2 text-base whitespace-pre">ID</th>
+                  <th className="px-4 py-2 text-base whitespace-pre">
+                    Document Type
+                  </th>
+                  <th className="px-4 py-2 text-base whitespace-pre">
+                    Full Name
+                  </th>
+                  <th className="px-4 py-2 text-base whitespace-pre">Phone</th>
+                  <th className="px-4 py-2 text-base whitespace-pre">
+                    Traning Type
+                  </th>
+                  <th className="px-4 py-2 text-base whitespace-pre">
+                    Created on
+                  </th>
+                  <th className="px-4 py-2 text-base whitespace-pre">DOB</th>
+                  <th className="px-4 py-2 text-base whitespace-pre">
+                    Address
+                  </th>
+                  <th className="px-4 py-2 text-base whitespace-pre">
+                    Fathers name
+                  </th>
+                  <th className="px-4 py-2 text-base whitespace-pre">
+                    Voucher id
+                  </th>
+                  <th className="px-4 py-2 text-base whitespace-pre">
+                    Document Image
+                  </th>
+                  <th className="px-4 py-2 text-base whitespace-pre">
+                    Profile Image
+                  </th>
+                  <th className="px-4 py-2 text-base whitespace-pre">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.traineedata.map((trainee) => (
+                  <tr>
+                    <td>
+                      <h3 className="px-4 py-2 text-sm whitespace-pre text-gray-400 hover:text-black cursor-pointer">
+                        {trainee.id}
+                      </h3>
+                    </td>
+                    <td>
+                      <h3 className="px-4 py-2 text-sm whitespace-pre text-gray-400 hover:text-black cursor-pointer">
+                        {trainee.doc_type}
+                      </h3>
+                    </td>
+                    <td>
+                      <h3 className="px-4 py-2 text-sm whitespace-pre text-gray-400 hover:text-black cursor-pointer">
+                        {trainee.name}
+                      </h3>
+                    </td>
+                    <td>
+                      <h3 className="px-4 py-2 text-sm whitespace-pre text-gray-400 hover:text-black cursor-pointer">
+                        {trainee.phone}
+                      </h3>
+                    </td>
+                    <td>
+                      <h3 className="px-4 py-2 text-sm whitespace-pre text-gray-400 hover:text-black cursor-pointer">
+                        {trainee.type}
+                      </h3>
+                    </td>
+                    <td>
+                      <h3 className="px-4 py-2 text-sm whitespace-pre text-gray-400 hover:text-black cursor-pointer">
+                        {trainee.created_on}
+                      </h3>
+                    </td>
+                    <td>
+                      <h3 className="px-4 py-2 text-sm whitespace-pre text-gray-400 hover:text-black cursor-pointer">
+                        {trainee.dob}
+                      </h3>
+                    </td>
+                    <td>
+                      <h3 className="px-4 py-2 text-sm whitespace-pre text-gray-400 hover:text-black cursor-pointer">
+                        {trainee.address}
+                      </h3>
+                    </td>
+                    <td>
+                      <h3 className="px-4 py-2 text-sm whitespace-pre text-gray-400 hover:text-black cursor-pointer">
+                        {trainee.fathers_name}
+                      </h3>
+                    </td>
+                    <td>
+                      <h3 className="px-4 py-2 text-sm whitespace-pre text-gray-400 hover:text-black cursor-pointer">
+                        {trainee.voucher_id}
+                      </h3>
+                    </td>
+                    <td>
+                      <img
+                        className="px-4 w-32 h-32 object-cover rounded-md"
+                        // src={
+                        //   "http://140.238.204.76:3001/" +
+                        //   trainee.doc_image.split("/")[1] +
+                        //   "/" +
+                        //   trainee.doc_image.split("/")[2]
+                        // }
+                        src={trainee.doc_image}
+                      />
+                    </td>
+                    <td>
+                      <img
+                        className="px-4 w-32 h-32 object-cover rounded-md"
+                        // src={
+                        //   "http://140.238.204.76:3001/" +
+                        //   trainee.profile_image.split("/")[1] +
+                        //   "/" +
+                        //   trainee.profile_image.split("/")[2]
+                        // }
+                        src={trainee.profile_image}
+                      />
+                    </td>
+                    <td>
+                      <div className="flex px-4 gap-2">
+                        <AiFillDelete
+                          className="cursor-pointer text-gray-400 text-xl hover:text-red-600"
+                          onClick={() => this.deleterecord(trainee.id)}
+                        />
 
-else if(this.state.traineedata!=[]){
-  return(
-    <div>
-      <CContainer fluid>
-      <Table name="datatable">
-  <thead>
-      <tr>
-          <th>ID</th>
-          <th>Document Type</th>
-          <th>full Name</th>
-          <th>Phone</th>
-          <th>Traning Type</th>
-          <th>Created on</th>
-          <th>DOB</th>
-          <th>Address</th>
-          <th>Fathers name</th>
-          <th>Voucher id</th>
-          <th>Document Image</th>
-          <th>Profile Image</th>
-      </tr>
-  </thead>
-  <tbody>
-      {
-      this.state.traineedata.map(trainee => (
-          <tr>
-              <td ><h3>{trainee.id}</h3></td>
-              <td ><h3>{trainee.doc_type}</h3></td>
-              <td ><h3>{trainee.name}</h3></td>
-              <td ><h3>{trainee.phone}</h3></td>
-              <td ><h3>{trainee.type}</h3></td>
-              <td ><h3>{trainee.created_on}</h3></td>
-              <td ><h3>{trainee.dob}</h3></td>
-              <td ><h3>{trainee.address}</h3></td>
-              <td ><h3>{trainee.fathers_name}</h3></td>
-              <td ><h3>{trainee.voucher_id}</h3></td>
-              <td ><img height="200px" width="200px" src={"http://140.238.204.76:3001/"+trainee.doc_image.split("/")[1]+"/"+trainee.doc_image.split("/")[2]}/></td>
-              <td ><img height="200px" width="200px" src={"http://140.238.204.76:3001/"+trainee.profile_image.split("/")[1]+"/"+trainee.profile_image.split("/")[2]}/></td>
-              <td > <h3 onClick={() =>this.deleterecord(trainee.id)}>Delete</h3></td>
-              <td ><h3  name="print" onClick={() =>this.printrow(trainee.id)}>Print</h3></td>
-          </tr>
-      ))}
-  </tbody>
-</Table>
+                        <AiFillPrinter
+                          className="cursor-pointer text-gray-400 text-xl hover:text-green-600"
+                          name="print"
+                          onClick={() => this.printrow(trainee.id)}
+                        />
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </CContainer>
 
-</CContainer>
-      
-{/* <div class="limiter">
+          {/* <div class="limiter">
   <div class="container-login200">
     <div class="wrap-login200">
     <span class="login100-form-title">
@@ -160,16 +264,10 @@ else if(this.state.traineedata!=[]){
 </div>
 </div>
 </div> */}
-
-    </div>
-  )
-
-}else{
-  
-  return (
-    <div>
-    </div>
-  )
-}
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
   }
 }
--- src/components/view.jsx
+++ src/components/view.jsx
@@ -1,155 +1,131 @@
-import React, { Component } from 'react'
-import axios  from 'axios';
-import { Navigate } from 'react-router-dom';
-import '@coreui/coreui/dist/css/coreui.min.css'
-import { CContainer,CRow,CCol } from '@coreui/react'
-
+import React, { Component } from "react";
+import axios from "axios";
+import { Navigate } from "react-router-dom";
+import "@coreui/coreui/dist/css/coreui.min.css";
+import { CContainer, CRow, CCol } from "@coreui/react";
 
 export default class view extends Component {
   constructor(props) {
-    super(props)
-  this.state = {
-         traineedata:[],
-         selected_id:'',
-         doc_image:'',
-         profile_image:'',
-         back:false
-    }
-}
-backhome=(e)=>{
-  e.preventDefault();
-  this.setState({
-    back:true
-  })
- 
-}
-componentDidMount=()=>{
-  var res=localStorage.getItem('selected');
-  let url='http://140.238.204.76:3001/trainee/'+res;
+    super(props);
+    this.state = {
+      traineedata: [],
+      selected_id: "",
+      doc_image: "",
+      profile_image: "",
+      back: false,
+    };
+  }
+  backhome = (e) => {
+    e.preventDefault();
+    this.setState({
+      back: true,
+    });
+  };
+  componentDidMount = () => {
+    var res = localStorage.getItem("selected");
+    let url = "http://140.238.204.76:3001/trainee/" + res;
 
-  axios.get(url)
-  .then((res) => {
-      this.setState({
+    axios
+      .get(url)
+      .then((res) => {
+        this.setState({
           traineedata: res.data[0],
-          doc_image:"http://140.238.204.76:3001/"+res.data[0].doc_image.split("/")[1]+"/"+res.data[0].doc_image.split("/")[2],
-          profile_image:"http://140.238.204.76:3001/"+res.data[0].profile_image.split("/")[1]+"/"+res.data[0].profile_image.split("/")[2]
-      });
-  }).catch((err) => console.log(err.response));
-}
-
+          doc_image:
+            "http://140.238.204.76:3001/" +
+            res.data[0].doc_image.split("/")[1] +
+            "/" +
+            res.data[0].doc_image.split("/")[2],
+          profile_image:
+            "http://140.238.204.76:3001/" +
+            res.data[0].profile_image.split("/")[1] +
+            "/" +
+            res.data[0].profile_image.split("/")[2],
+        });
+      })
+      .catch((err) => console.log(err.response));
+  };
 
   render() {
+    if (this.state.back == true) {
+      return <Navigate to="/home" />;
+    } else {
+      if (this.state.traineedata != []) {
+        console.log("rendering");
+        console.log("DOC IMAGW" + this.state.doc_image);
+        console.log("profile IMAGW" + this.state.profile_image);
+        return (
+          <div className="w-full p-8">
+            <CContainer className="border border-gray-500 shadow-lg rounded-lg p-4 bg-gray-50">
+              <CContainer>
+                {/* <CRow className="align-self-start">
+                  <CCol>Hello</CCol>
+                </CRow> */}
 
-if(this.state.back==true){
-  return <Navigate to='/home'/>
-}
-else{
+                <CRow className="align-items-start">
+                  <CCol>
+                    <span class="login100-form-title">
+                      Ace Institute and Traning Center
+                    </span>
+                  </CCol>
+                </CRow>
+                <CRow className="align-items-start">
+                  <CCol>Ilam Municipality, Ilam</CCol>
+                </CRow>
 
-  if(this.state.traineedata!=[]){
+                <CRow className="align-items-center">
+                  <CCol>Trainee Data</CCol>
 
+                  <CCol>Submitted Documents</CCol>
+                </CRow>
 
-    console.log("rendering");
-    console.log("DOC IMAGW"+this.state.doc_image);
-    console.log("profile IMAGW"+this.state.profile_image);
-    return (
-      <div>
-<CContainer>
+                <CRow className="align-self-start">
+                  <CCol></CCol>
+                  <CCol>Photo </CCol>
+                </CRow>
 
-<CContainer>
-{/* <CRow className="align-self-start">
+                <CRow className="align-self-start">
+                  <CCol>
+                    <CRow>Name: {this.state.traineedata.name}</CRow>
+                    <CRow>
+                      Document Type: {this.state.traineedata.doc_type}
+                    </CRow>
+                    <CRow>Date of Birth: {this.state.traineedata.dob}</CRow>
+                    <CRow>Phone : {this.state.traineedata.phone}</CRow>
+                    <CRow>Traning type: {this.state.traineedata.type}</CRow>
+                    <CRow>Address: {this.state.traineedata.address}</CRow>
+                    <CRow>
+                      Fathers Name: {this.state.traineedata.fathers_name}
+                    </CRow>
+                  </CCol>
+                  <CCol>
+                    <img
+                      src={this.state.profile_image}
+                      width="200px"
+                      height="200px"
+                      alt="profile_image"
+                    />
+                  </CCol>
+                </CRow>
 
-<CCol>Hello</CCol>
-
-</CRow> */}
-
-  <CRow className="align-items-start">
-
-    <CCol><span class="login100-form-title">
-      Ace Institute and Traning Center
-        </span></CCol>
-
-  </CRow>
-  <CRow className="align-items-start">
-
-    <CCol>Ilam Municipality, Ilam</CCol>
-
-  </CRow>
-
-
-  <CRow className="align-items-center">
-
-    <CCol>Trainee Data</CCol>
-
-    <CCol>Submitted Documents</CCol>
-
-    </CRow>
-
-
-    <CRow className="align-self-start">
-
-    <CCol>
-    
-    </CCol>
-    <CCol>Photo </CCol>
-    </CRow>
-
-
-    <CRow className="align-self-start">
-
-<CCol>
-  <CRow>
-    Name: {this.state.traineedata.name}
-  </CRow>
-  <CRow>
-  Document Type: {this.state.traineedata.doc_type}
-    </CRow>
-    <CRow>
-    Date of Birth: {this.state.traineedata.dob}
-    </CRow>
-    <CRow>
-    Phone : {this.state.traineedata.phone}
-    </CRow>
-    <CRow>
-    Traning type: {this.state.traineedata.type}
-    </CRow>
-    <CRow>
-    Address: {this.state.traineedata.address}
-    </CRow>
-    <CRow>
-    Fathers Name: {this.state.traineedata.fathers_name}
-    </CRow>
-</CCol>
-<CCol><img src={this.state.profile_image} width="200px" height="200px"/></CCol>
-</CRow>
-
-
-
-<CRow className="align-self-start">
-
-<CCol>
-{this.state.traineedata.doc_type } Image
-</CCol>
-</CRow>
-<CRow className="align-self-start">
-</CRow>
-<CRow className="align-self-center">
-<CCol>
-<img src={this.state.doc_image}  width="600px" height="600px"/>
-</CCol>
-</CRow>
-</CContainer>
-
-</CContainer>
-
-      </div>
-    )
-  
-  }
-
-}
-
-
-
-
+                <CRow className="align-self-start">
+                  <CCol>{this.state.traineedata.doc_type} Image</CCol>
+                </CRow>
+                <CRow className="align-self-start"></CRow>
+                <CRow className="align-self-center">
+                  <CCol>
+                    <img
+                      src={this.state.doc_image}
+                      width="600px"
+                      height="600px"
+                      alt="document_image"
+                    />
+                  </CCol>
+                </CRow>
+              </CContainer>
+            </CContainer>
+          </div>
+        );
+      }
+    }
   }
 }
